88099b0e4d878a269f7e94b1ba456108ab7beb3e
Sal Tamay <sal.tamay@outlook.com>
2020-02-20 17:06:32 -0500

    feat: Add env variables managed by dotenv

--- app.py
+++ app.py
@@ -2,13 +2,22 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import sqlalchemy
 from flask_cors import CORS
 import os
+from dotenv import load_dotenv
+# ...
+
+# load dotenv in the base root
+# refers to application_top
+basedir = os.path.join(os.path.dirname(__file__), '..')
+dotenv_path = os.path.join(basedir, '.env')
+load_dotenv(dotenv_path)
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DEV_DATABASE_URI'] = os.getenv('DEV_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 @app.route('/', methods=['GET'])
 def test():
     return jsonify({
-        "success": True
+        "success": True,
     })
