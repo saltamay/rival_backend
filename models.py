7a94b9f11b553a555036876cae7c547f032f746a
Sal Tamay <sal.tamay@outlook.com>
2020-02-20 19:23:24 -0500

    test: Add create_all() script to create models and tables

--- models.py
+++ models.py
@@ -11,6 +11,7 @@ def setup_db(app, database_path=db_path):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
+    db.create_all()
 
 
 class Bootcamp(db.Model):
